ce23ed2a20750c9106398cf5f03eaf1a239a0c09
sweetArizCider <arizah.applecider+github@gmail.com>
2025-03-11 17:56:11 -0600

    Finished :)!

--- index.js
+++ index.js
@@ -1,30 +1,17 @@
-// index.js
-// where your node app starts
-
-// init project
 require('dotenv').config();
-var express = require('express');
-var app = express();
-
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
+const express = require('express');
+const app = express();
 var cors = require('cors');
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.get('/api/whoami', (req, res) => {
+  const ip = req.ip
+  const language = req.headers["accept-language"];
+  const userAgent = req.headers["user-agent"];
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.status(200).json({ipaddress: ip, language : language, software: userAgent});
 });
 
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
-});
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+const listener = app.listen(process.env.PORT || 3000,() => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
